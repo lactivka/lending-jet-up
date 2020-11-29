7b5970eb60f1093f135c014d7687084cbd9ef840
unknown <lactivo4ka@gmail.com>
2020-11-29 18:34:02 +0200

    feat: make social icons react component, change icon color on hover, implement user comment switcher

--- test-lending/src/components/Comments/index.jsx
+++ test-lending/src/components/Comments/index.jsx
@@ -2,9 +2,27 @@ import { useState } from 'react';
 import { comments } from '../../constants/content';
 import './index.scss';
 import userPhoto from '../../assets/images/user_photo.png';
+import { ReactComponent as Facebook } from '../../assets/icons/facebook.svg';
+import { ReactComponent as Instagram } from '../../assets/icons/instagram.svg';
+import { ReactComponent as Twitter } from '../../assets/icons/twitter.svg';
+import { ReactComponent as VK } from '../../assets/icons/vk.svg';
 
 function Comments() {
-  const [selectedUser /*, setSelectedUser*/] = useState('user1');
+  const [selectedUser, setSelectedUser] = useState('user1');
+  const [hoveredIcon, setHoveredIcon] = useState(null);
+
+  const onClick = (event) => {
+    setSelectedUser(event.target.id);
+  }
+
+  const onHover = (event) => {
+    setHoveredIcon(event.target.id);
+  }
+
+  const onLeave = (event) => {
+    setHoveredIcon(null);
+  }
+
   return (
     <section className="comments">
       <div className="comments-content">
@@ -16,18 +34,53 @@ function Comments() {
               <p className="user-name">{comments[selectedUser].name}</p>
               <p className="user-position">{comments[selectedUser].position}</p>
               <div className="social-icons">
-                <div className="social-icon facebook"></div>
-                <div className="social-icon instagram"></div>
-                <div className="social-icon twitter"></div>
-                <div className="social-icon vk"></div>
+                <Facebook
+                  className={`social-icon ${hoveredIcon === 'facebook' ? 'hovered' : null}`}
+                  id="facebook"
+                  onMouseOver={onHover}
+                  onMouseOut={onLeave}
+                />
+                <Instagram
+                  className={`social-icon ${hoveredIcon === 'instagram' ? 'hovered' : null}`}
+                  id="instagram"
+                  onMouseOver={onHover}
+                  onMouseOut={onLeave}
+                />
+                <Twitter
+                  className={`social-icon ${hoveredIcon === 'twitter' ? 'hovered' : null}`}
+                  id="twitter"
+                  onMouseOver={onHover}
+                  onMouseOut={onLeave}
+                />
+                <VK
+                  className={`social-icon ${hoveredIcon === 'vk' ? 'hovered' : null}`}
+                  id="vk"
+                  onMouseOver={onHover}
+                  onMouseOut={onLeave}
+                />
               </div>
             </div>
           </div>
           <div className="comment-text">{comments[selectedUser].comment}</div>
           <div className="switcher">
-            <div className={`bullet ${selectedUser === 'user1' ? 'selected' : null}`} id="user1"></div>
-            <div className={`bullet ${selectedUser === 'user2' ? 'selected' : null}`} id="user2"></div>
-            <div className={`bullet ${selectedUser === 'user3' ? 'selected' : null}`} id="user3"></div>
+            <div
+              className={`bullet ${selectedUser === 'user1' ? 'selected' : null}`}
+              id="user1"
+              onClick={onClick}
+            >
+            </div>
+            <div
+              className={`bullet ${selectedUser === 'user2' ? 'selected' : null}`}
+              id="user2"
+              onClick={onClick}
+            >
+            </div>
+            <div
+              className={`bullet ${selectedUser === 'user3' ? 'selected' : null}`}
+              id="user3"
+              onClick={onClick}
+            >
+            </div>
           </div>
         </div>
       </div>
